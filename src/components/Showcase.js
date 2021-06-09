93baeeaacddae1536a09077f5c0e987d5945705f
jany-coder <jany_ete@yahoo.com>
2021-06-09 20:46:06 +0600

    portfolio complete

---
+++ src/components/Showcase.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Showcase = () => {
+    return <>
+        <section className="showcase">
+            <div className="overlay flex flex-col items-center justify-center text-white px-5">
+                <h1 className="text-5xl font-bold text-right mb-10 lg:text-center lg:text-7xl lg:px-56">I create <span className="text-green-500">brand awareness</span> for small business</h1>
+
+                <div className="">
+                    <Link to="/hire" className="mr-5 bg-green-500 p-2 px-4 rounded-full font-semibold hover:bg-green-800 transition-all">
+                        Get Started
+                    </Link>
+                    <Link to="/projects" className="mr-5 bg-white text-green-500 p-2 px-4 rounded-full font-semibold hover:bg-green-800 hover:text-white transition-all">Projects
+                    </Link>
+                </div>
+            </div>
+        </section>
+    </>
+};
+
+export default Showcase;
---
+++ src/data.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        id: 1,
+        path: './images/consultant.jpg',
+        desc: 'Startup Consultant'
+
+    },
+    {
+        id: 2,
+        path: './images/food.png',
+        desc: 'Hungry Monster'
+
+    },
+    {
+        id: 3,
+        path: './images/bazar.png',
+        desc: 'Fresh Bazar'
+
+    },
+    {
+        id: 4,
+        path: './images/portfolio.png',
+        desc: 'Portfolio'
+
+    }
+]
+
+export default data
